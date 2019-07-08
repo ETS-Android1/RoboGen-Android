01be3e7beaa5b30bc5301587d69bc15037359b57
Mathias Schmoigl <48010492+SRFG-MAT@users.noreply.github.com>
2019-07-08 12:53:54 +0200

    Add files via upload
    Started Android RoboGen Project, initial checkin

---
+++ RoboGen/app/src/main/java/at/srfg/robogen/ItemContent.java
@@ -0,0 +1,60 @@
+package at.srfg.robogen;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ */
+public class ItemContent {
+
+    /**
+     * Members for Item creation
+     */
+    public static final List<ItemEntry> ITEMS = new ArrayList<ItemEntry>();
+    public static final Map<String, ItemEntry> ITEM_MAP = new HashMap<String, ItemEntry>();
+    private static final int COUNT = 25;
+
+    static {
+        // Add some sample items.
+        addItem(new ItemEntry("1:", "Hugo", "Management-Seite für Q.Bo One - Roboter"));
+        addItem(new ItemEntry("2:", "Uhren", "Management-Seite für Fitbit Charge 2 - Smart Watches"));
+        addItem(new ItemEntry("3:", "Alexa", "Management-Seite für Alexa Entscheidungsbäume"));
+    }
+
+    private static void addItem(ItemEntry item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    private static String makeDetails(int position) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Details about Item: ").append(position);
+        for (int i = 0; i < position; i++) {
+            builder.append("\nMore details information here.");
+        }
+        return builder.toString();
+    }
+
+    /**
+     * An item representing a piece of content.
+     */
+    public static class ItemEntry {
+        public final String id;
+        public final String content;
+        public final String details;
+
+        public ItemEntry(String id, String content, String details) {
+            this.id = id;
+            this.content = content;
+            this.details = details;
+        }
+
+        @Override
+        public String toString() {
+            return content;
+        }
+    }
+}
