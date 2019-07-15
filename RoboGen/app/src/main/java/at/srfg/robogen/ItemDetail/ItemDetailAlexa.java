1d1e887ad2dbc0ba1e0e48f32081a7cd28a76862
Mathias Schmoigl <mathias.schmoigl@salzburgresearch.at>
2019-07-15 11:36:43 +0200

    added Text and Buttons for Alexa Detail Page and Smartwatch Detail Page

--- RoboGen/app/src/main/java/at/srfg/robogen/ItemDetail/ItemDetailAlexa.java
+++ RoboGen/app/src/main/java/at/srfg/robogen/ItemDetail/ItemDetailAlexa.java
@@ -4,11 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import androidx.core.content.res.ResourcesCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import at.srfg.robogen.R;
 
 public class ItemDetailAlexa extends ItemDetailBase {
+
+    private final String mText_1 = "Test-Schritt) Verbindung aufbauen mit Alexa:";
+    public FloatingActionButton mAlexaStartButton;
 
     /*******************************************************************************
      * creating view for alexa detail page
@@ -17,12 +25,39 @@ public class ItemDetailAlexa extends ItemDetailBase {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.item_detail_alexa, container, false);
+        final View mainView = LayoutInflater.from(inflater.getContext()).inflate(R.layout.item_list_content, container, false);
 
         // Show the dummy content as text in a TextView.
         if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.item_detail_title)).setText(mItem.header);
+            initGUIComponents(rootView, mainView);
         }
 
         return rootView;
+    }
+
+    /*******************************************************************************
+     * init GUI components
+     ******************************************************************************/
+    private void initGUIComponents(final View rootView, final View mainView){
+
+        ((TextView) rootView.findViewById(R.id.item_detail_title)).setText(mItem.header);
+        ((TextView) rootView.findViewById(R.id.item_detail_text_1)).setText(mText_1);
+
+
+        mAlexaStartButton = (FloatingActionButton) rootView.findViewById(R.id.bt_search);
+        mAlexaStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Starte Alexa..", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                //-------------------------------------------------------
+                // TODO: test funktioniert noch nicht
+                ImageView image = (ImageView) mainView.findViewById(R.id.image_connOverview);
+                image.setImageResource(R.drawable.img_connected);
+                image.setImageDrawable(ResourcesCompat.getDrawable(getResources(),R.drawable.img_connected, null));
+                //-------------------------------------------------------
+            }
+        });
     }
 }
--- RoboGen/app/src/main/java/at/srfg/robogen/ItemDetail/ItemDetailWatch.java
+++ RoboGen/app/src/main/java/at/srfg/robogen/ItemDetail/ItemDetailWatch.java
@@ -4,11 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import androidx.core.content.res.ResourcesCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import at.srfg.robogen.R;
 
 public class ItemDetailWatch extends ItemDetailBase {
+
+    private final String mText_1 = "Test-Schritt) Verbindung aufbauen mit Uhr:";
+    public FloatingActionButton mWatchStartButton;
 
     /*******************************************************************************
      * creating view for watch detail page
@@ -17,12 +25,38 @@ public class ItemDetailWatch extends ItemDetailBase {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.item_detail_watch, container, false);
+        final View mainView = LayoutInflater.from(inflater.getContext()).inflate(R.layout.item_list_content, container, false);
 
         // Show the dummy content as text in a TextView.
         if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.item_detail_title)).setText(mItem.header);
+            initGUIComponents(rootView, mainView);
         }
 
         return rootView;
+    }
+
+    /*******************************************************************************
+     * init GUI components
+     ******************************************************************************/
+    private void initGUIComponents(final View rootView, final View mainView){
+
+        ((TextView) rootView.findViewById(R.id.item_detail_title)).setText(mItem.header);
+        ((TextView) rootView.findViewById(R.id.item_detail_text_1)).setText(mText_1);
+
+        mWatchStartButton = (FloatingActionButton) rootView.findViewById(R.id.bt_search);
+        mWatchStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Starte Verbindung mit Uhr..", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                //-------------------------------------------------------
+                // TODO: test funktioniert noch nicht
+                ImageView image = mainView.findViewById(R.id.image_connOverview);
+                image.setImageResource(R.drawable.img_connected);
+                image.setImageDrawable(ResourcesCompat.getDrawable(getResources(),R.drawable.img_connected, null));
+                //-------------------------------------------------------
+            }
+        });
     }
 }
